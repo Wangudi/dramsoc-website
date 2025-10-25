9b112ee95c51e80261287bcd66f3c8cd7e687d62
Wangudi <wangudilameck18@gmail.com>
2025-10-25 18:31:12 +0300

    Fix: add missing pages for deployment

---
+++ src/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div className="contact">
+      <h1>Contact Us</h1>
+      <p>Get in touch with the Drama Society.</p>
+    </div>
+  );
+}
+
+export default Contact;
