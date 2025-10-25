dcabad3540f87551f7681766fb196fb9c31374e6
Wangudi <wangudilameck18@gmail.com>
2025-10-25 18:33:14 +0300

    Fix: add missing pages for deployment

---
+++ src/pages/Partners.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Partners() {
+  return (
+    <div className="partners">
+      <h1>Our Partners</h1>
+      <p>Meet our valued partners and sponsors.</p>
+    </div>
+  );
+}
+
+export default Partners;
