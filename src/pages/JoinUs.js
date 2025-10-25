bebdffd6975802ea61a53ca1748364a570e55f6e
Wangudi <wangudilameck18@gmail.com>
2025-10-25 18:32:15 +0300

    Fix: add missing pages for deployment

---
+++ src/pages/JoinUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function JoinUs() {
+  return (
+    <div className="join-us">
+      <h1>Join Us</h1>
+      <p>Become part of the Drama Society family.</p>
+    </div>
+  );
+}
+
+export default JoinUs;
